3f522215b9c3546c312b15af4bc2e365a2091762
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2020-12-25 16:37:27 +0100

    Multiplying and dividing app

--- app.js
+++ app.js
@@ -2,20 +2,39 @@
 
 let $score;
 
-
-
 // variables
 
 const h1 = document.querySelector('h1');
 const buttonPomnoz = document.querySelector('.pomnoz');
 const buttonPodziel = document.querySelector('.podziel');
 let oryginalnyWynik = document.querySelector('.wynik');
-let nowyWynik = document.querySelector('.nowyWynik');
+let nowyWynik = document.querySelector('.nowywynik');
 
+//addition function
+// and score determination
 
 let dodawanie = function(a,b) {
-   return $score = a + b;
+  return  $score = a + b;
+  oryginalnyWynik.innerText = `${$score}`;
 }
 
-console.log(dodawanie(2,3));
+//multiplying function 
 
+function mnozenie (a,b) {
+  nowyWynik.textContent = $score * 2;
+}
+//dividing function 
+
+function dzielenie (a,b){
+nowyWynik.textContent = $score/2
+}
+
+mnozenie();
+dzielenie();
+
+//buttons listeners 
+
+buttonPomnoz.addEventListener('click', mnozenie)
+buttonPodziel.addEventListener('click', dzielenie)
+
+dodawanie(2,9);
